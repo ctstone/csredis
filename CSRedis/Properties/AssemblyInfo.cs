580c80cd80627eb5528478f6211faaf181e7d160
Asbjørn Clemmensen <ac@givetwise.dk>
2016-11-27 19:26:59 +0100

    Bumping versions to avoid conflics with already published packages

--- CSRedis/Properties/AssemblyInfo.cs
+++ CSRedis/Properties/AssemblyInfo.cs
@@ -22,8 +22,8 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("1e4063d6-d412-4c26-83c5-63a29ca84334")]
 
-[assembly: AssemblyVersion("3.1.0")]
-[assembly: AssemblyFileVersion("3.1.0")]
-[assembly: AssemblyInformationalVersion("3.1.0-rc1")]
+[assembly: AssemblyVersion("3.3.0")]
+[assembly: AssemblyFileVersion("3.3.0")]
+[assembly: AssemblyInformationalVersion("3.3.0-rc1")]
 //[assembly: InternalsVisibleTo("CSRedis.Tests, PublicKey=0024000004800000940000000602000000240000525341310004000001000100b7e70b54a9e7596d195fd76f95b0fc80f53158f828c72a84b153168637072cb94f8f8d8f574948ad1c4267f1a2e7feb31df0adcb97f46156bcc32c022278a589f9d21b79d3a5cce62a68497fd4abc96daeb7f1d8dbf204d95b6dcccec52e2ed274d8b7bacd4db0a381f9301832fdedcf52eaa66ff2d7867f8fd7f5c932b055aa")]
 [assembly: InternalsVisibleTo("CSRedis.Tests")]
